1a00282db6e2b9bd8a47092511cfd2d40225f8e8
TomasNagy <tomi.nagy@outlook.com>
2023-05-26 17:07:29 +0200

    Implement linear search algorithm

--- linear_search.c
+++ linear_search.c
@@ -8,4 +8,18 @@ int main(void)
     int numbers[ARRAY_LENGTH] = {0, 500, 10, 50, 100, 300, 200};
 
     int get_user_number = get_int("Number: ");
+
+    // Linear search throught the array
+    // Time complexity O(n)
+    for (int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        if (numbers[i] == get_user_number) 
+        {
+            printf("Found!\n");
+            return 0;
+        }
+    }
+
+    printf("Not found!\n");
+    return 1;
 }
